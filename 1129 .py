014c045db558ef7754e45dd02866251d171869e2
Seonggyu-art <dydtjdrb@naver.com>
2024-11-29 13:56:42 +0900

    1129. first python

---
+++ 1129 .py
@@ -0,0 +1,12 @@
+# print("hello, world")
+# print("hello", "python", sep=" " )
+# print( "dkssudflasd", 44)
+# print( "안녕하세요.", end="")
+# print( "코딩온입니다." )
+print("1")
+print("안녕혀세요.", end="\n")
+print("코딩온입니다.")
+ive = "I AM"
+print(ive)
+ive ="장원영"
+print(ive)
