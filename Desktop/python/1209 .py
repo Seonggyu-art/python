a98c8731e4dcab5a7a208e28616e7bc8ffd6e311
Seonggyu-art <dydtjdrb@naver.com>
2024-12-09 13:25:36 +0900

    12월 9일 교육내용

---
+++ Desktop/python/1209 .py
@@ -0,0 +1,23 @@
+# # 클래스
+# a = dict()
+# a = set()
+
+# class b:
+#     pass  # 붕어빵 틀
+
+# c = b()  # c 가 객채, 붕어빵임.
+
+# bb1 = b()
+# bb2 = b()
+# bb3 = b()   # 뿡어빵 찍어내기
+
+# class Movie:
+#     title = "범죄도시4"
+
+# movie1 = Movie()
+# movie2 = Movie()
+
+# print(movie2.title)
+# print(movie2.title)
+
+print(" 테스트 실행")
