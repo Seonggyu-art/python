d0f9933824273ff10200dc7752a1275f39ff4606
Seonggyu-art <dydtjdrb@naver.com>
2024-12-10 17:10:05 +0900

    12월 교육 내용 포함사항

---
+++ module1.py
@@ -0,0 +1,15 @@
+# import calc_module  # calc_module 이라는 파일로 바로 갈 수 있음. 컨트롤 선택 하면 됨.
+# # 주의 사항 .py는 붙이지 말기
+# print(calc_module.add(1,2))  # 기존의 calc_module에 있는 것들을 사용할 수 있게 됨. 팀전때 사용
+# print(calc_module.sub(1,2))
+# print(calc_module.mul(1,2))
+# print(calc_module.div(1,2))
+
+# from calc_module import add # add 만 가져왔음.
+
+# print(add(1,2))
+# # calc_module.add() # 안됨.
+
+# import calc_module as cm   # 이름이 길때? 중복된 것이 있을 때 다른 이름으로 변경해서 사용.
+# print(cm.add(1,2))
+
